53a64a330cc4d9cad2400d437c92189ddbf909b4
Akash kumar <akashk808443@gmail.com>
2022-06-06 18:14:37 +0530

    ok

--- index.js
+++ index.js
@@ -1,72 +1,108 @@
 const express = require("express");
-const { v4: uuid } = require("uuid");
+const nanoid = require("nanoid");
+
 const fs = require("fs");
 
 const app = express();
-const port = 5000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello i am akash");
-});
-
-
-app.get("/users", (req, res) => {
-  fs.readFile("./db.json", { encoded: "utf-8" }, (err, data) => {
-    // res.setHeaders("content-type", "application/json");
-    res.send(data);
-  });
-});
-
 
 app.post("/user/create", (req, res) => {
-  console.log(req.body);
-  fs.readFile("./db.json", { encoded: "utf-8" }, (err, data) => {
-    
+  fs.readFile("./db.json", "utf-8", (err, data) => {
     const parsed = JSON.parse(data);
     parsed.users = [...parsed.users, req.body];
 
+    fs.writeFile("./db.json", JSON.stringify(parsed), "utf-8", () => {
+      res.status(201).send("user created");
+    });
   });
 });
 
+//Auth
 app.post("/user/login", (req, res) => {
-
-    fs.writeFile(
-      "./db.json",
-      JSON.stringify(parsed),
-      { encoded: "utf-8" },
-      () => {
-        res.status(201).send(`Login Successful ${uuid()} ${flag}`);
-      }
-    );
-  });
-});
+  if (!req.body.username || !req.body.password) {
+    return res
+      .status(400)
+      .send(`{ status: "please provide username and password" }`);
+  }
 
 
-app.post("/user/login/:id", (req, res) => {
-  let { id } = req.params();
-  let flag = false;
-  //   console.log(req.body);
-  fs.readFile("./db.json", { encoded: "utf-8" }, (err, data) => {
-    //preserving old data
+  fs.readFile("./db.json", "utf-8", (err, data) => {
     const parsed = JSON.parse(data);
-
     parsed.users = parsed.users.map((el) =>
-      el.id == id ? { ...el, token: "" } : el
+      req.body.username == el.username && req.body.password == el.password
+        ? { ...el, token: Math.random() }
+        : el
     );
 
-    fs.writeFile(
-      "./db.json",
-      JSON.stringify(parsed),
-      { encoded: "utf-8" },
-      () => {
-        res.send(`user logged out successfully`);
-      }
-    );
+
+    fs.writeFile("./db.json", JSON.stringify(parsed), "utf-8", () => {
+      res.status(201).send("login successful");
+    });
   });
 });
 
-app.listen(port, () => {
-  console.log(`server is started on port http://localhost:${port}/`);
+
+app.post("/user/logout", (req, res) => {
+  res.send("user logged out successfully");
 });
+
+
+app.get("/votes/party/:party", (req, res) => {
+  const { party } = req.params;
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    const parsed = JSON.parse(data);
+    parsed.users = parsed.users.filter((el) => el.party === party);
+    res.send(JSON.stringify(parsed.users));
+  });
+});
+
+
+
+app.get("/votes/voters", (req, res) => {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    const parsed = JSON.parse(data);
+    parsed.users = parsed.users.filter((el) => el.role === "voter");
+    res.send(JSON.stringify(parsed.users));
+  });
+});
+
+
+app.post("/votes/vote/:user", (req, res) => {
+  const { user } = req.params;
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    const parsed = JSON.parse(data);
+    parsed.users = parsed.users.map((t) =>
+      t.name == user ? { ...t, votes: 1 } : t
+    );
+
+    
+    fs.writeFile("./db.json", JSON.stringify(parsed), "utf-8", () => {
+      res.send("user voted");
+    });
+  });
+});
+
+
+app.get("/db", (req, res) => {
+    fs.readFile("./db.json", "utf-8", (err, data) => {
+        const parsed = JSON.parse(data);
+        parsed.users = [...parsed.users, req.body];
+    
+        fs.writeFile("./db.json", JSON.stringify(parsed), "utf-8", () => {
+          res.send("user added");
+        });
+      });
+})
+
+
+app.post("/db", (req, res) => {
+    fs.readFile("./db.json", "utf-8", (err, data))
+})
+
+
+
+
+app.listen(8080);
